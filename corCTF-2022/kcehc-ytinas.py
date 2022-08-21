0a5cbdef406b551781a13a3f72cd9efcf6bfff40
MarioChao <89794497+MarioChao@users.noreply.github.com>
2022-08-22 01:28:42 +0800

    Create kcehc-ytinas.py
    Writeup for sanity check 2022!

---
+++ corCTF-2022/kcehc-ytinas.py
@@ -0,0 +1,8 @@
+# Challenge description:
+# This challenge has nothing hidden or tricky about it - just type what you see in the image above.
+"""
+  1. Open the image in an image viewing software
+  2. Rotate the image 180º
+  3. The text on the flipped image reads "flag: corctf{}"
+"""
+print("corctf{}")
